23edd36af4cbb61337386da073a3d9caa56c811d
A5TA <cardillojamesbrandon@gmail.com>
2024-03-15 16:45:12 -0400

    added the project and modified task,team

---
+++ backend/src/main/java/com/agile/project/models/ProjectComponents/ProjectRepository.java
@@ -0,0 +1,6 @@
+package com.agile.project.models.ProjectComponents;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Integer> {
+}
